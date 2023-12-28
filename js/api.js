a74e2cfd5f52771f12030012ed7f668f0d8cd320
Hasmik <99020792+hunanyanhasmik@users.noreply.github.com>
2023-12-28 14:43:04 +0400

    Weather app with pure js

--- js/api.js
+++ js/api.js
@@ -3,7 +3,6 @@ export const getWeatherData = async (city) => {
         const response = await fetch(
             `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=36b7e482b92303025ab4eb5d0d1a6757&units=metric`
         );
-
         return await response.json();
     } catch (error) {
         console.error(error);
